812e70f0e59dfb3d3136351f278f06ea9cc07187
undervolt <93976452+Undervoltologist@users.noreply.github.com>
2026-01-17 14:18:11 +0300

    Update Fan_Update_CPU_TACH.c

--- ec/thermal/Fan_Update_CPU_TACH.c
+++ ec/thermal/Fan_Update_CPU_TACH.c
@@ -4,7 +4,7 @@ void Fan_Update_CPU_TACH(void)
 {
   undefined extraout_R6;
   undefined extraout_R7;
-  char cVar1;
+  char raw_ticks;
   
   FUN_RAM_015e21();
   nop();
@@ -13,17 +13,17 @@ void Fan_Update_CPU_TACH(void)
   nop();
   TACH_Switch = REG_TSWCTLR & 0xd;
   if ((TACH_Switch >> 2 & 1) != 1) {
-    FUN_RAM_01a6c7();
-    cVar1 = MainEC_CPU_Fan_TACH_LO;
+    thunk_FUN_RAM_015e21();
+    raw_ticks = MainEC_CPU_Fan_TACH_LO;
     if (MainEC_CPU_Fan_TACH_LO == '\0') {
-      cVar1 = MainEC_CPU_Fan_TACH_HI;
+      raw_ticks = MainEC_CPU_Fan_TACH_HI;
     }
-    if ((cVar1 == '\0') || (func_0x01a6d3(), cVar1 == '\0')) {
-      cVar1 = MainEC_CPU_Fan_TACH_LO;
+    if ((raw_ticks == '\0') || (FUN_RAM_01a6d3(), raw_ticks == '\0')) {
+      raw_ticks = MainEC_CPU_Fan_TACH_LO;
       if (MainEC_CPU_Fan_TACH_LO == '\0') {
-        cVar1 = MainEC_CPU_Fan_TACH_HI;
+        raw_ticks = MainEC_CPU_Fan_TACH_HI;
       }
-      if (cVar1 == '\0') {
+      if (raw_ticks == '\0') {
         DAT_RAM_00045f = DAT_RAM_00045f & 0xef;
       }
       CPU_TACH_LO = 0;
