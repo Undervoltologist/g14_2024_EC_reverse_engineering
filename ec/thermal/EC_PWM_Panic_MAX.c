fd4bdee2d5da7914469bb13e188aff0d883bcda9
undervolt <93976452+Undervoltologist@users.noreply.github.com>
2026-01-25 09:36:00 +0300

    Update EC_PWM_Panic_MAX.c

--- ec/thermal/EC_PWM_Panic_MAX.c
+++ ec/thermal/EC_PWM_Panic_MAX.c
@@ -2,7 +2,7 @@
 void Fan_PWM_Panic_MAX(void)
 
 {
-  if (((DAT_RAM_00045e >> 5 & 1) != 0) && ((DAT_RAM_000484 >> 2 & 1) != 0)) {
+  if (((Thermal_Status >> 5 & 1) != 0) && ((Fan_Control_Toggle >> 2 & 1) != 0)) {
     Fan_CPU_PWM_Target = REG_CTR0;
     REG_DCR4 = REG_CTR0;
     Fan_GPU_PWM_Target = REG_CTR0;
