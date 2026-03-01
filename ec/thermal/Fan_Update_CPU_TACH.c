0febc46eda9074fdec1350ea12af15e23cb3ad36
undervolt <93976452+Undervoltologist@users.noreply.github.com>
2026-03-01 15:04:54 +0300

    ok

--- ec/thermal/Fan_Update_CPU_TACH.c
+++ ec/thermal/Fan_Update_CPU_TACH.c
@@ -4,40 +4,41 @@ void Fan_Update_CPU_TACH(void)
 {
   undefined extraout_R6;
   undefined extraout_R7;
-  char raw_ticks;
+  uint8_t in_ACC;
+  uint8_t ACC:1;
   
-  FUN_RAM_015e21();
+  Init_Sensors(0x68b,in_ACC);
   nop();
   nop();
   nop();
   nop();
-  TACH_Switch = REG_TSWCTLR & 0xd;
-  if ((TACH_Switch >> 2 & 1) != 1) {
-    thunk_FUN_RAM_015e21();
-    raw_ticks = MainEC_CPU_Fan_TACH_LO;
-    if (MainEC_CPU_Fan_TACH_LO == '\0') {
-      raw_ticks = MainEC_CPU_Fan_TACH_HI;
+  Calc_68a = REG_TSWCTLR & 0xd;
+  if ((Calc_68a >> 2 & 1) != 1) {
+    Init_Sensors(0x366,REG_F1TMRR);
+    ACC:1 = MainEC_CPU_Fan_TACH.low_byte;
+    if (MainEC_CPU_Fan_TACH.low_byte == '\0') {
+      ACC:1 = MainEC_CPU_Fan_TACH.high_byte;
     }
-    if ((raw_ticks == '\0') || (FUN_RAM_01a6d3(), raw_ticks == '\0')) {
-      raw_ticks = MainEC_CPU_Fan_TACH_LO;
-      if (MainEC_CPU_Fan_TACH_LO == '\0') {
-        raw_ticks = MainEC_CPU_Fan_TACH_HI;
+    if ((ACC:1 == '\0') || (FUN_RAM_01a6d3(), ACC:1 == '\0')) {
+      ACC:1 = MainEC_CPU_Fan_TACH.low_byte;
+      if (MainEC_CPU_Fan_TACH.low_byte == '\0') {
+        ACC:1 = MainEC_CPU_Fan_TACH.high_byte;
       }
-      if (raw_ticks == '\0') {
-        DAT_RAM_00045f = DAT_RAM_00045f & 0xef;
+      if (ACC:1 == '\0') {
+        Fan_State = Fan_State & 0xef;
       }
       CPU_TACH_LO = 0;
       CPU_TACH_HI = 0;
     }
-    else if ((TACH_Switch >> 3 & 1) != 0) {
-      DAT_RAM_00045f = DAT_RAM_00045f | 0x10;
-      Init_Fan_Control(MainEC_CPU_Fan_TACH_LO);
+    else if ((Calc_68a >> 3 & 1) != 0) {
+      Fan_State = Fan_State | 0x10;
+      Init_Fan_Control(MainEC_CPU_Fan_TACH.low_byte);
       FUN_RAM_01a734();
       func_0x015e15();
       FUN_RAM_015dfd();
       CPU_TACH_LO = extraout_R6;
       CPU_TACH_HI = extraout_R7;
-      REG_TSWCTLR = TACH_Switch;
+      REG_TSWCTLR = Calc_68a;
       return;
     }
   }
